2d97043bb74022cd891baa807e45021209d2a5ae
Winfrey Ward Wekesa <winfreywekesa148@gmail.com>
2026-05-14 16:44:57 +0300

    feat: Add fetchTasks function to load tasks from API

--- src/App.jsx
+++ src/App.jsx
@@ -6,3 +6,18 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState("");
+
+  
+  useEffect(() => {
+    async function fetchTasks() {
+      try {
+        const response = await fetch(
+          "http://localhost:3000/tasks"
+        );
+
+        if (!response.ok) {
+          throw new Error(
+            `HTTP Error: ${response.status}`
+          );
+        }
+
