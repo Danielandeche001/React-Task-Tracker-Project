7d812b9f4ce49d85604b92428081fafe22087e2f
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-05-14 19:41:15 +0300

    Feat:Adjustments to the html elements in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import './App.css'
 import TaskForm from './components/TaskForm'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
         <div>
-          <h1>Get started with Task Tracker</h1>
+          <h1>Welcome to Task Tracker</h1>
           <br></br>
           <br></br>
           <p>
             Keep track of your tasks and stay organized with our Task Tracker app. 
+            </p>
+            <p>
             Add new tasks, set priorities, and manage your to-do list efficiently.
           </p>
           <br></br>
