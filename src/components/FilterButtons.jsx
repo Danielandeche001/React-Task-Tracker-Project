beac6b098048868218061dc7f19f6aca7298f0d6
sirkiruthi <orencaroli@gmail.com>
2026-05-14 16:57:24 +0300

    Feat add changes to FilterButton.jsx

--- src/components/FilterButtons.jsx
+++ src/components/FilterButtons.jsx
@@ -1,1 +1,23 @@
-function FilterButtons({ setFilter })
+function FilterButtons({ setFilter }) {
+  return (
+    <div>
+      <button onClick={() => setFilter("All")}>
+        All
+      </button>
+
+      <button onClick={() => setFilter("Low")}>
+        Low
+      </button>
+
+      <button onClick={() => setFilter("Medium")}>
+        Medium
+      </button>
+
+      <button onClick={() => setFilter("High")}>
+        High
+      </button>
+    </div>
+  );
+}
+
+export default FilterButtons;
